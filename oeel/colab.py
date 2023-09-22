63bf6c12bded1eb4a9e091f4fd4ea6660044aa56
Mathieu Gravey <mathieu@mgravey.com>
2023-09-22 21:35:16 +0200

    redesign credential system to manage refreshing

--- oeel/colab.py
+++ oeel/colab.py
@@ -1,8 +1,8 @@
 import ee
 import IPython
 from google.colab import output
-import google.auth.credentials
-import threading
+from google.oauth2.credentials import Credentials
+from datetime import datetime, timedelta
 import time
 
 class OEE_ExtensionError(Exception):
@@ -10,54 +10,63 @@ class OEE_ExtensionError(Exception):
 		self.message = message
 		super().__init__(self.message)
 
-def AuthAndInitilize(extensionID="dhkobehdekjgdahfldleahkekjffibhg"):
-	jsCode='''
-		window.authToken = "";
+class OEE_Credentials(Credentials):
+	def __init__(self, extensionID, *args, **kwargs):
+		super().__init__("",*args, **kwargs)
+		self.startJsSectionToRecoverTokens(extensionID);
+		self.setNewToken(True);
+		
 
-		function sendAuthMessage() {
-			chrome.runtime.sendMessage("''' + extensionID + '''", "getAuthTocken",
-				function(response) {
-					if(response && response.type=='authToken'){
-						window.authToken = response.message;
-					}else{
-						window.authToken='error';
+	def startJsSectionToRecoverTokens(self,extensionID):
+		jsCode='''
+			window.authToken = "";
+
+			function sendAuthMessage() {
+				chrome.runtime.sendMessage("''' + extensionID + '''", "getAuthTocken",
+					function(response) {
+						if(response && response.type=='authToken'){
+							window.authToken = response.message;
+				console.log(window.authToken)
+				localStorage.setItem("EEauthToken",response.message)
+						}else{
+							window.authToken='error';
+				localStorage.setItem("EEauthToken",'error')
+						}
 					}
-				}
-			);
-		}
+				);
+			}
 
-		// Call the function once immediately
-		sendAuthMessage();
-		// Then call it every 14 minutes
-		setInterval(sendAuthMessage, 14 * 60 * 1000);
-	'''
-	IPython.display.display(IPython.display.Javascript(jsCode))
+			// Call the function once immediately
+			sendAuthMessage();
+			// Then call it every 14 minutes
+			setInterval(sendAuthMessage, 14 * 60 * 1000);
+		'''
+		IPython.display.display(IPython.display.Javascript(jsCode))
 
-	def fetch_auth_token():
-		return output.eval_js('authToken')
-	token=fetch_auth_token()
-	cpt=0
-	while token=='' and cpt<1000:
-		token = fetch_auth_token()	
-		time.sleep(0.01)
-		cpt+=cpt+1
-	if(token=='error'):
-		raise OEE_ExtensionError("The token could not be retrieved from the extension.");
-	if(token!=''):
-		credential=google.oauth2.credentials.Credentials(token)
-		ee.Initialize(credential)
-	else:
-		raise OEE_ExtensionError("Unexpected error during retrieval from the extension.")
+	def getToken(self):
+		for _ in range(10):
+			tk=output.eval_js('localStorage.getItem("EEauthToken")',False,1);
+			if tk is not None:
+				return tk
+		return None
 
-	def monitor_auth_token():
-		while True:
-			time.sleep(5 * 60)
-			token = fetch_auth_token()
-			credential=google.oauth2.credentials.Credentials(token)
-			ee.data._credentials=credential;
+	def setNewToken(self,initialize=False):
+		token=self.getToken()
+		if(initialize):
+			cpt=0
+			while token=='' and cpt<1000:
+				token=self.getToken()	
+				time.sleep(0.01)
+				cpt+=cpt+1
+			if(token=='error'):
+				raise OEE_ExtensionError("The token could not be retrieved from the extension.");
+		self.token = token;
+		self.expiry = datetime.utcnow() + timedelta(minutes=30)  # Token valid for 1 hour but let's refresh each 30 min
 
-	# Create a thread to monitor the authToken
-	monitor_thread = threading.Thread(target=monitor_auth_token)
+	def refresh(self, request):
+		if self.expired:
+			self.setNewToken();
 
-	# Start the thread
-	monitor_thread.start()
+
+def AuthAndInitilize(extensionID="dhkobehdekjgdahfldleahkekjffibhg"):
+	ee.Initialize(OEE_Credentials(extensionID))
